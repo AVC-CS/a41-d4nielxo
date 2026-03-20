442aac3f2e5667e127aefd292853e77dda833eab
d4nielxo <dhaddad3@students.avc.edu>
2026-03-20 14:45:15 -0700

    commit done

--- main.cpp
+++ main.cpp
@@ -1,46 +1,44 @@
-
-//?  [Problem Definition]
-//*        A software company sells a package that retails for $99. Quantity discounts are given
-//*        according to the following table.
-//*                Quantity                 Discount Percent
-//*                10 - 19                        20%
-//*                20 - 49                        30%
-//*                50 - 99                        40%
-//*                100+                            50%
-//*- Write a program that asks for the number of units sold and
-//*  computes the total cost of the purchase.
-//** Input Validation : Input must be a positive integer.
-//**    If we have negative input, just quit the program.
-
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-int main()
-{
-  const double PERC19 = 0.2;
-  const double PERC49 = 0.3;
-  const double PERC99 = 0.4;
-  const double PERC100 = 0.5;
-  const double Price = 99.00;
-  double totalCost, originalAmount, discountAmount, dRate;
-  int numberofSold;
+int main() {
+    const double PERC19  = 0.2;
+    const double PERC49  = 0.3;
+    const double PERC99  = 0.4;
+    const double PERC100 = 0.5;
+    const double Price   = 99.00;
 
-  cout << "Enter the number of units sold\n";
-  // TODO
+    double totalCost, originalAmount, discountAmount, dRate;
+    int numberofSold;
 
-  // Input Validation
-  // TODO
+    cout << "Enter the number of units sold\n";
+    cin >> numberofSold;
 
-  // Determine discount rate based on quantity
-  // TODO
+    
+    if (numberofSold < 0) {
+        cout << "Error: number of units must be positive.\n";
+        return 0;
+    }
 
-  // Calculate original amount, discount, and total
-  // TODO
+    if (numberofSold < 10) {
+        dRate = 0.0;
+    } else if (numberofSold <= 19) {
+        dRate = PERC19;
+    } else if (numberofSold <= 49) {
+        dRate = PERC49;
+    } else if (numberofSold <= 99) {
+        dRate = PERC99;
+    } else { 
+        dRate = PERC100;
+    }
 
-  /* To print out your result, use the following statements */
-  cout << setprecision(2) << fixed;
-  cout << "Original amount is " << originalAmount << endl;
-  cout << "Discount amount is " << discountAmount << endl;
-  cout << "Total Price is " << totalCost << endl;
+    originalAmount = numberofSold * Price;
+    discountAmount = originalAmount * dRate;
+    totalCost      = originalAmount - discountAmount;
+
+    cout << setprecision(2) << fixed;
+    cout << "Original amount is " << originalAmount << endl;
+    cout << "Discount amount is " << discountAmount << endl;
+    cout << "Total Price is " << totalCost << endl;
 }
